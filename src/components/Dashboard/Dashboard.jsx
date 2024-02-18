50a4ac14cd11e223e9bb4b9bd66bf3e859be5511
Zarrar Hussain Abid <zarrarabid72@gmail.com>
2024-02-18 21:22:47 +0500

    error resolved at initial Stage after loading

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -126,7 +126,7 @@ const Dashboard = () => {
                       <div className="task-inner-container-2">
                         <div>
                           <p className=" days">
-                            {task.days.length > 1 ? `${task.days[0]} - ${task.days[task.days.length - 1]}` : task.days[0].length < 3 ? `${task.days[0]} days` : task.days[0]}</p>
+                            {task?.days?.length > 1 ? `${task?.days[0]} - ${task?.days[task?.days?.length - 1]}` : task?.days[0]?.length < 3 ? `${task?.days[0]} days` : task?.days[0]}</p>
                           <p className="task-name">{task.taskName}</p>
                         </div>
                         <button
