b9d183e91e81c79879f001e280534d0520f37235
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-02-18 21:32:22 +0530

    local Storage issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Navbar from "./components/Navbar/Navbar";
 import './App.css'
+import { Todo_context } from "./components/Context/Context";
 
 const App = () => {
+  const { loading } = React.useContext(Todo_context);
   return (
-    <div className="app-wrapper">
-      <Navbar />
-      <div className="todo-body">
-        <Sidebar />
-        <Dashboard />
-      </div>
-    </div>
+    <>
+      {
+        loading ? <div class="lds-ring"><div></div><div></div><div></div><div></div></div> :
+          <div className="app-wrapper">
+            <Navbar />
+            <div className="todo-body">
+              <Sidebar />
+              <Dashboard />
+            </div>
+          </div>
+
+      }
+    </>
   );
 };
 
